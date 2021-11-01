7fd00f77d4dae0c9ef52f05dacfc5201aeb3a22b
Myungho Lee <leedox@naver.com>
2021-11-01 20:12:30 +0900

    ENTEC 추가

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("leedox/", include("leedox.urls"))
+    path("leedox/", include("leedox.urls")),
+    path("entec/", include("entec.urls")),
 ]
