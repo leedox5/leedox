361191a73dc1f7af3148d51abc7fb9a6f1a2954e
Myungho Lee <leedox@naver.com>
2021-11-01 11:17:09 +0900

    [2021.11.01] VSCODE 한글

--- leedox/views.py
+++ leedox/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("환영합니다, Hello, Welcome to LEEDOX!")
+    return HttpResponse("환영합니다, Hello, Welcome to LEEDOX!!!")
