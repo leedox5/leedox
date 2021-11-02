f2b9d84f7580d1f05662a4f54e417a3539908de8
이명호 <leedox@naver.com>
2021-11-02 21:57:57 +0900

    게임생성

--- entec/urls.py
+++ entec/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "entec"
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('detail', views.detail, name="detail"),
+    path('game/create', views.game_create, name="game_create"),
+    path('player/create', views.player_create, name="player_create"),
 ]
--- entec/views.py
+++ entec/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "entec/game_list.html")
+
+def detail(request):
+    return render(request, "entec/game_detail.html")
+
+def game_create(request):
+    return render(request, "entec/game_form.html")
+
+def player_create(request):
+    return render(request, "entec/player_form.html")
