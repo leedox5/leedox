cc8224dfaf3c54ff763badf805f519580ab7bc79
이명호 <leedox@naver.com>
2021-11-09 15:55:19 +0900

    host 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.165.198.195', 'app.leedox.kr']
+ALLOWED_HOSTS = ['15.165.198.195', 'app.leedox.kr', '20.119.61.239']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
