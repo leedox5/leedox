0ac16c2697e251683dba1ccaa263349b4da5b6d9
Myungho Lee <leedox@naver.com>
2022-09-17 08:44:13 +0900

    SYNC

--- entec/admin.py
+++ entec/admin.py
@@ -6,8 +6,3 @@ from entec.models import Match
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(Match)
-
-class MatchAdmin(admin.ModelAdmin):
-    search_fields = ['desc']
-
-admin.site.register(Match, MatchAdmin)
