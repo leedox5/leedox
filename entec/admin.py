e152974cdce129d979a8d2433549967cbf79da1b
Myungho Lee <leedox@naver.com>
2022-09-17 08:15:44 +0900

    ADMIN

--- entec/admin.py
+++ entec/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from entec.models import Game
+from entec.models import Match
 
 # Register your models here.
 admin.site.register(Game)
+admin.site.register(Match)
